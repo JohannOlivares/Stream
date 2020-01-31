237967e5fef8a9f10a1beb0ed8dc936dfb85177f
Johann Olivares <johannolivares102@gmail.com>
2020-01-30 19:40:45 -0800

    Initial Commit

---
+++ Stream/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stream
+//
+//  Created by JOHANN OLIVARES on 1/30/20.
+//  Copyright © 2020 JOHANN OLIVARES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
